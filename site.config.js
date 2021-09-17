23ffefb99a20990d66484d2b771f5eb42e799299
MHC-I <69659094+MHC-I@users.noreply.github.com>
2021-09-17 22:03:52 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -29,7 +29,7 @@ module.exports = {
   imageCDNHost: null,
 
   // Utteranc.es comments via GitHub issue comments (optional)
-  utterancesGitHubRepo: 'MHC-I/nextjs-notion-starter-kit',
+  utterancesGitHubRepo: null,
 
   // whether or not to enable support for LQIP preview images (optional)
   // NOTE: this requires you to set up Google Firebase and add the environment
