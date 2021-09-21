bc1c27b3a7bdbe31e67d9b5d3a576dc57a7c41dd
MHC-I <69659094+MHC-I@users.noreply.github.com>
2021-09-21 10:20:13 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,7 +26,10 @@ export const Footer: React.FC<{
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2021 {config.author}</div>
+      <div className={styles.copyright}>
+        <a href="https://www.upyun.com/?utm_source=lianmeng&amp;utm_medium=referra"><img src="https://cdn.jsdelivr.net/gh/MHC-I/nextjs-notion-starter-kit/public/upyun.png" align="absmiddle" width="59px" height="30px" /></a>
+
+      </div>
 
       {hasMounted ? (
         <div className={styles.settings}>
